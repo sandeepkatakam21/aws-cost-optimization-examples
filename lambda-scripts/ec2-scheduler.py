5a8951149350aa0e9bfc5e6b0035706146308850
sandeepkatakam21 <118243260+sandeepkatakam21@users.noreply.github.com>
2025-09-10 09:25:35 +0530

    Add lambda-scripts folder with EC2 scheduler placeholderAdd lambda-scripts folder with EC2 scheduler placeholderCreate ec2-scheduler.py

---
+++ lambda-scripts/ec2-scheduler.py
@@ -0,0 +1,8 @@
+# EC2 Scheduler Lambda Function
+# This script will contain the Lambda function code for automating EC2 start/stop operations
+# to optimize costs by scheduling instances based on usage patterns.
+
+# TODO: Implement Lambda handler function
+# TODO: Add EC2 start/stop logic
+# TODO: Add CloudWatch Events integration
+# TODO: Add error handling and logging
